3131be6e3cbfa5b0515853248fe0bc9a8c008e0f
mariana-b-alves <129863863+mariana-b-alves@users.noreply.github.com>
2023-04-04 18:34:46 +0100

    V.2 (+Glass)

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-var openBtn = document.querySelector(".open-btn");
-var closeBtn = document.querySelector(".close-btn");
-var popup = document.querySelectorAll(".popup");
-
-
-openBtn.addEventListener('click', function (){
-    for(let x = 0; x < popup.length; x++ ){
-        popup[x].classList.add('visible');
-    }
-})
-
-closeBtn.addEventListener('click', function (){
-    for(let x = 0; x < popup.length; x++ ){
-        popup[x].classList.remove('visible');
-    }
-})
+alert("Sign up here")
